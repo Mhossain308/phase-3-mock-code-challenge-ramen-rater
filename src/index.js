0b82dfeaf6a458c58f49b4ded9b59a8e76e1f1a5
Mhossain308 <Mz.hossain212@gmail.com>
2021-03-23 13:49:55 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 // write your code here
+const ramenMenu = document.querySelector('div#ramen-menu')
+
+
+function renderAllRamens(){
+    fetch('http://localhost:3000/ramens')
+    .then(response => response.json())
+    .then(ramenObject => {ramenObject.forEach(ramen => {renderOneRamen(ramen)})
+ })
+
+}
+
+renderAllRamens()
+
+function renderOneRamen(ramenObject) {
+    const ramenImg = document.createElement('img')
+    ramenImg.dataset.id = ramenObject.id
+
+    ramenImg.src = ramenObject.image
+    console.log(ramenMenu)
+    ramenMenu.append(ramenImg)
+   
+
+    
+}
+
+
+
+
